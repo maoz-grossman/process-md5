3b5ceabcff97c77cab5c6a2a856688a87ced457e
Maoz-Grossman <44970530+maoz-grossman@users.noreply.github.com>
2019-06-13 18:58:44 +0300

    done :-)

--- signals.c
+++ signals.c
@@ -3,7 +3,21 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <stdlib.h>
+//----------------------------------------
+//****************************************
+//              os ex 1.3
+//________________________________________
+//
+// parent and child processes comunication
+// using signls.
+// to compile: gcc -o signals signals.c
+// to run: ./signals <enter your string>
+//****************************************
+//----------------------------------------
 
+//---------------------------------------
+//          global var
+//---------------------------------------
 int processes[5];
 int index;
 
@@ -16,7 +30,9 @@ int sigCatcher(){
 		kill(processes[index],SIGINT);
 }
 
-
+//---------------------------------------
+//            main function
+//---------------------------------------
 int main(){
 	int status;
 	int pid;
@@ -25,7 +41,7 @@ int main(){
 	signal(SIGINT, sigCatcher);
 	
 	//main loop
-	while(counter<5){
+	for(counter=0; counter<5; counter++){
 		if((pid=fork())<0){//eror
 			printf("eror in fork");
 			exit(1);
@@ -38,19 +54,18 @@ int main(){
 		}
 		else//parent
 			processes[counter] = pid;
-		counter++;
 	}
 	
-	sleep(1);
+	sleep(2);
 	kill(processes[4],SIGINT);
-	sleep(1);
+	sleep(2);
 	
 	int collectChild;
 	counter=0;
 	
 	while(counter<5){
-		collectChild = wait(&status);
-		printf("%d id dead\n",collectChild);
+		collectChild = wait(&status); 		// collect zombies
+		printf("%d is dead\n", collectChild);
 		counter++;
 	}
 	exit(0);
